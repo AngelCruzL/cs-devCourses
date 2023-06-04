afa1baa1c114cba6b3a24f8712e466f53ec9d1b0
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-06-03 22:29:22 -0600

    Add Price entity

---
+++ Domain/Entities/Price.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+
+public class Price : BaseEntity
+{
+  public decimal CurrentPrice { get; set; }
+  public decimal Discount { get; set; }
+  public int CourseId { get; set; }
+}
